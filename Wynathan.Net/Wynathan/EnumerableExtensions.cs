be3f368d495624d510a6693204baa52a61eee3b8
Vlad Glyebov <glyebov.vo@gmail.com>
2018-04-24 16:48:22 +0300

    [Common]: provided a common class library to add general functionality there

--- Wynathan.Net/Wynathan/EnumerableExtensions.cs
+++ Wynathan.Net/Wynathan/EnumerableExtensions.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-namespace Wynathan.Net.Extensions
+namespace Wynathan
 {
     public static class EnumerableExtensions
     {
---
+++ Wynathan.Net/Wynathan/ExceptionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Wynathan
+{
+    public static class ExceptionExtensions
+    {
+        public static string Format(this Exception ex)
+        {
+            if (ex == null)
+                return string.Empty;
+
+            var currentMessage = string.Format("{0}{1}{2}{1}", ex.Message,
+                Environment.NewLine, Format(ex.InnerException));
+
+            var aggrException = ex as AggregateException;
+            if (aggrException != null)
+            {
+                var aggrMessageBuilder = new StringBuilder();
+                aggrMessageBuilder.Append(currentMessage);
+
+                foreach (var inner in aggrException.InnerExceptions)
+                {
+                    aggrMessageBuilder.Append(string.Format("{0}{1}{2}{1}",
+                        inner.Message, Environment.NewLine, Format(inner.InnerException)));
+                }
+
+                return aggrMessageBuilder.ToString();
+            }
+
+            return currentMessage;
+        }
+    }
+}
